2246476e997f439bd957b74ff1413a662d9e381c
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-20 20:19:57 -0300

    WIP: Type checking with Flow

--- src/components/FeedItem.jsx
+++ src/components/FeedItem.jsx
@@ -1,33 +1,31 @@
+// @flow
 import React from 'react';
-import PropTypes from 'prop-types';
+
+
+type Item = {
+  feed: {
+    title: string,
+    description: string,
+    link: string,
+  }
+}
 
 const defaultProps = {
-  feed: {
-    link: '',
-    description: '',
-  },
-};
+  link: '',
+  description: '',
+}
 
-const propTypes = {
-  feed: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    link: PropTypes.string,
-    description: PropTypes.string,
-  }),
-};
-
-const FeedItem = props => (
+const FeedItem = (item: Item) => (
   <div className="row marketing">
     <div className="col-lg-12">
-      <a href={props.feed.link}>
-        <h4>{props.feed.title}</h4>
+      <a href={item.feed.link}>
+        <h4>{item.feed.title}</h4>
       </a>
-      <p>{props.feed.description}</p>
+      <p>{item.feed.description}</p>
     </div>
   </div>
 );
 
-FeedItem.propTypes = propTypes;
 FeedItem.defaultProps = defaultProps;
 
 export default FeedItem;
